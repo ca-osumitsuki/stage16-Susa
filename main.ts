f3f7bc53188091b63875e0f4502a9c357642fece
ca-osumitsuki <157934286+ca-osumitsuki@users.noreply.github.com>
2024-02-11 11:48:04 +0900

    0211

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`ダイヤモンド`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
+    music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.InBackground)
+    music.setVolume(255)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`チェスト_開いてない`, function (sprite, location) {
-    stage = 1
+    stage += 1
     init_stage(stage)
 })
 function init_stage (stage: number) {
@@ -9,7 +15,27 @@ function init_stage (stage: number) {
     } else if (stage == 1) {
         tiles.setCurrentTilemap(tilemap`レベル_2`)
         tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 10))
+    } else if (stage == 2) {
+        tiles.setCurrentTilemap(tilemap`レベル_3`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 10))
+    } else if (stage == 3) {
+        tiles.setCurrentTilemap(tilemap`レベル_3`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 10))
     }
+}
+function damage () {
+    info.changeLifeBy(-1)
+    mySprite.startEffect(effects.fire)
+    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
+    mySprite.vy = -150
+    music.setVolume(120)
+    music.play(music.melodyPlayable(music.jumpDown), music.PlaybackMode.InBackground)
+    controller.moveSprite(mySprite, 0, 0)
+    pause(2000)
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+    effects.clearParticles(mySprite)
+    init_stage(stage)
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.tileKindAt(TileDirection.Bottom, assets.tile`ブロック1`)) {
@@ -18,7 +44,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let mySprite: Sprite = null
 let stage = 0
-info.setLife(5)
+info.setScore(0)
+info.setLife(3)
 stage = 0
 mySprite = sprites.create(assets.image`いぬたぬき_右`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
@@ -34,12 +61,16 @@ game.onUpdate(function () {
 })
 forever(function () {
     if (mySprite.tilemapLocation().row >= 15) {
-        info.changeLifeBy(-1)
-        mySprite.startEffect(effects.fire)
-        scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
-        pause(2000)
-        scene.cameraFollowSprite(mySprite)
-        effects.clearParticles(mySprite)
-        init_stage(stage)
+        damage()
+    }
+})
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Bottom, assets.tile`トゲ1`)) {
+        damage()
+    }
+})
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Top, assets.tile`トゲ0`)) {
+        damage()
     }
 })
